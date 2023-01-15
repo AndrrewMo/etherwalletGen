6bdba3a55111e4b226d52824303847118c1ff880
AndrrewMo <122011155+AndrrewMo@users.noreply.github.com>
2023-01-15 11:39:26 +0100

    main.java
    my first commit

---
+++ main.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class main {
+  public static void main(String[] args) {
+   Scanner scanner = new Scanner(System.in); 
+    int y = scanner.nextInt();
+    System.out.println("Введите количество кошельков для генерации" + y);
+  }
+}
